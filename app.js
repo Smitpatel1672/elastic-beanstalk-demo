3686e92ae4f23f82081146348112f07ac79a7687
Smitpatel1672 <66589447+Smitpatel1672@users.noreply.github.com>
2024-11-22 14:21:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
                 <title>Hello World</title>
             </head>
             <body>
-                <h1>Hello World</h1>
+                <h1>Hello World - 1</h1>
             </body>
         </html>
     `);
