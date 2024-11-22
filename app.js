a63bd670d7ad9b8cb97a4155b92e01f7490d9e1e
Smitpatel1672 <66589447+Smitpatel1672@users.noreply.github.com>
2024-11-22 13:40:01 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// Set up a simple route
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <title>Hello World</title>
+            </head>
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </html>
+    `);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
