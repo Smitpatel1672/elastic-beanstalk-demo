8ae6d6c49952c2b8db63c02bcccf439b5730ae58
Smitpatel1672 <66589447+Smitpatel1672@users.noreply.github.com>
2024-11-27 13:29:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
 
 // Set up a simple route 
 app.get('/', (req, res) => {
