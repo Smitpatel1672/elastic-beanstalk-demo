e1b90fd162c587346a4cdb98fee8fc7163e7b848
Smitpatel1672 <66589447+Smitpatel1672@users.noreply.github.com>
2024-11-27 13:39:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get("/health", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
